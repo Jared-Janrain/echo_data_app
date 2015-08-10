928f0629c00ab32f8cd4dab17bb75ddcf1ecca85
Jared Stensland <jstensland@janrain.com>
2015-08-10 13:20:14 -0700

    handle JSON files that are sent

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,23 @@
 class StaticPagesController < ApplicationController
+
+  skip_before_filter :verify_authenticity_token, only: [:show_data]
+
   def home
   end
 
   def show_data
     @data = params
+    @file
+    @file_hash = {}
+    params.each do |key, value|
+      # byebug
+      if value.class == ActionDispatch::Http::UploadedFile
+        @file = value
+        @file_hash = JSON.parse(@file.read)
+
+      end
+    end
+
     render 'show_data'
   end
 
